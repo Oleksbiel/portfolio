4f4f10caa06097cecd9503766775e6af19c64fa5
Sasha <Oleksbielianinov@gmail.com>
2016-08-17 17:20:29 +0300

    add skils bar

--- markup/js/assets.js
+++ markup/js/assets.js
@@ -1,1 +1,11 @@
 $('.portfolio__img').responsiveEqualHeightGrid();
+
+// Progress Bar
+
+jQuery(document).ready(function(){
+	jQuery('.skills__row').each(function(){
+		jQuery(this).find('.skills__bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},3000);
+	});
+});
