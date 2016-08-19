87a3e6866bb1c9c6b01bef16a3f0a90f69fc380a
vsleibenko <v.s.leibenko@gmail.com>
2016-08-19 15:02:38 +0300

    Refactored JS v1.5

--- production/js/assets.js
+++ production/js/assets.js
@@ -145,3 +145,30 @@ function updatePosition() {
   // add your code to update the position when your browser
   // is resized or scrolled
 }
+
+// Progress Bar
+
+jQuery(document).ready(function(){
+	jQuery('.skills__row').each(function(){
+		jQuery(this).find('.skills__bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},3000);
+	});
+});
+
+// Hamburger
+
+$(document).ready(function() {
+	var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
+    var hamburgers = document.querySelectorAll(".hamburger");
+    if (hamburgers.length > 0) {
+      forEach(hamburgers, function(hamburger) {
+        hamburger.addEventListener("click", function() {
+          this.classList.toggle("is-active");
+        }, false);
+      });
+    }
+	$('.hamburger').click(function(){
+		$('.header__nav-burger').slideToggle(1000);
+	});
+});
