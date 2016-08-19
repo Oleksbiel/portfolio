ce753d2dab2d3906109b8738c3f7745468d5e105
Sasha <Oleksbielianinov@gmail.com>
2016-08-19 10:14:49 +0300

    Started  mobile ver.

--- production/js/assets.js
+++ production/js/assets.js
@@ -1,93 +1,5 @@
 $('.portfolio__img').responsiveEqualHeightGrid();
 
-function HeaderListScroll(list) {
-	if (!list) return;
-	this.list = list;
-	this.scrollDone = true;
-	this.scrollTime = 3;
-	this.list.addEventListener("click", this.computeProps.bind(this));
-};
-
-HeaderListScroll.prototype.computeProps = function() {
-	event.preventDefault();
-	if (event.target.tagName != "A") return;
-	if (!this.scrollDone) return;
-	this.scrollDone = false;
-	var allBlocks = document.querySelectorAll(".block"),
-		position;
-	for (var i = 0; i < allBlocks.length; i++) {
-		if (allBlocks[i].dataset.name == event.target.innerHTML) {
-			position = getPosition(allBlocks[i]);
-			this.scrollingTo(position.y);
-			this.undoneScrollProtection(position.y);
-			return;
-		};
-	};
-};
-
-HeaderListScroll.prototype.scrollingTo = function(destination) {
-	var $this = this,
-		step;
-	if (destination % 5 != 0) {
-		destination = destination - (destination % 5);
-	};
-	step = (destination > 0) ? 5 : -5;
-	if (destination == 0) return;
-	setTimeout(function() {
-		window.scrollBy(0, step);
-		destination -= step;
-		$this.scrollingTo(destination);
-	}, $this.scrollTime);
-};
-
-HeaderListScroll.prototype.undoneScrollProtection = function(position) {
-	var $this = this,
-		time = (position / 5 * $this.scrollTime * 1.25);
-	time = (time > 0) ? time : -time;
-	setTimeout(function() {
-		$this.scrollDone = true;
-	}, time);
-};
-
-new HeaderListScroll(document.querySelector(".header__list"));
-
-
-// Helper function to get an element's exact position
-function getPosition(el) {
-  var xPos = 0;
-  var yPos = 0;
- 
-  while (el) {
-    if (el.tagName == "BODY") {
-      // deal with browser quirks with body/window/document and page scroll
-      var xScroll = el.scrollLeft || document.documentElement.scrollLeft;
-      var yScroll = el.scrollTop || document.documentElement.scrollTop;
- 
-      xPos += (el.offsetLeft - xScroll + el.clientLeft);
-      yPos += (el.offsetTop - yScroll + el.clientTop);
-    } else {
-      // for all other non-BODY elements
-      xPos += (el.offsetLeft - el.scrollLeft + el.clientLeft);
-      yPos += (el.offsetTop - el.scrollTop + el.clientTop);
-    }
- 
-    el = el.offsetParent;
-  }
-  return {
-    x: xPos,
-    y: yPos
-  };
-}
- 
-// deal with the page getting resized or scrolled
-window.addEventListener("scroll", updatePosition, false);
-window.addEventListener("resize", updatePosition, false);
- 
-function updatePosition() {
-  // add your code to update the position when your browser
-  // is resized or scrolled
-}
-
 // Progress Bar
 
 jQuery(document).ready(function(){
@@ -95,5 +7,22 @@ jQuery(document).ready(function(){
 		jQuery(this).find('.skills__bar').animate({
 			width:jQuery(this).attr('data-percent')
 		},3000);
+	});
+});
+
+// Hamburger
+
+$(document).ready(function() {
+	var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
+    var hamburgers = document.querySelectorAll(".hamburger");
+    if (hamburgers.length > 0) {
+      forEach(hamburgers, function(hamburger) {
+        hamburger.addEventListener("click", function() {
+          this.classList.toggle("is-active");
+        }, false);
+      });
+    }
+	$('.hamburger').click(function(){
+		$('.header__nav-burger').slideToggle(1000);
 	});
 });
